03f93b4a5ee9dc9bb545e57b827443e390a0b46d
Daniel Chicot <dan.chicot@gmail.com>
2021-03-31 09:44:22 +0100

    sync

--- src/main/kotlin/uk/gov/dwp/dataworks/snapshot/mapreduce/TableScanMapper.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/snapshot/mapreduce/TableScanMapper.kt
@@ -14,7 +14,8 @@ class TableScanMapper: TableMapper<Text, Text>() {
         CompoundProcessor.processor.process(result)?.let { sanitisedRecord ->
             val row = result.row
             val f: Int = if (row[0] < 0) (row[0] + 256) else row[0].toInt()
-            val key = String.format("db.${context.configuration["source.table"].replace(":", "_")}_%03d", f)
+            val g: Int = if (row[1] < 0) (row[1] + 256) else row[1].toInt()
+            val key = String.format("db.${context.configuration["source.table"].replace(":", "_")}_%03d_%03d", f, g)
             val dbObject = Text().apply { set(decryptedRecord(sanitisedRecord)) }
             context.write(Text().apply { set(key) }, dbObject)
         }
--- src/main/kotlin/uk/gov/dwp/dataworks/snapshot/tool/Exporter.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/snapshot/tool/Exporter.kt
@@ -19,44 +19,47 @@ import java.util.*
 class Exporter: Configured(), Tool {
 
     override fun run(args: Array<out String>): Int =
-            initialisedTableMapperJob(args[0], args[1], args[2].toInt()).run {
-                if (waitForCompletion(true)) {
-                    0
-                } else {
-                    log.error("Job failed")
-                    1
-                }
+        initialisedTableMapperJob(args[0], args[1], args[2].toInt(), args[3].toInt()).run {
+            if (waitForCompletion(true)) {
+                0
+            } else {
+                log.error("Job failed")
+                1
             }
+        }
 
-    private fun initialisedTableMapperJob(sourceTable: String, targetBucket: String, reducerCount: Int): Job =
-            job(sourceTable, targetBucket, reducerCount).also { job ->
-                TableMapReduceUtil.initTableMapperJob(sourceTable,
-                    scan(),
-                    TableScanMapper::class.java,
-                    Text::class.java,
-                    Text::class.java,
-                    job)
-            }
+    private fun initialisedTableMapperJob(sourceTable: String, targetBucket: String, reducerCount: Int,
+                                          batchSize: Int): Job =
+        job(sourceTable, targetBucket, reducerCount).also { job ->
+            TableMapReduceUtil.initTableMapperJob(sourceTable,
+                scan(batchSize),
+                TableScanMapper::class.java,
+                Text::class.java,
+                Text::class.java,
+                job)
+            TableMapReduceUtil.setScannerCaching(job, batchSize)
+        }
 
-    private fun scan(): Scan =
-            Scan().apply {
-                caching = 500
-                cacheBlocks = false
-            }
+    private fun scan(batchSize: Int): Scan =
+        Scan().apply {
+            caching = batchSize
+            cacheBlocks = false
+            maxResultSize = -1
+        }
 
     private fun job(sourceTable: String, targetBucket: String, reducerCount: Int): Job =
-            Job.getInstance(configuration(sourceTable, targetBucket), jobName(sourceTable)).apply {
-                setJarByClass(TableScanMapper::class.java)
-                outputFormatClass = NullOutputFormat::class.java
-                reducerClass = S3Reducer::class.java
-                numReduceTasks = reducerCount
-            }
+        Job.getInstance(configuration(sourceTable, targetBucket), jobName(sourceTable)).apply {
+            setJarByClass(TableScanMapper::class.java)
+            outputFormatClass = NullOutputFormat::class.java
+            reducerClass = S3Reducer::class.java
+            numReduceTasks = reducerCount
+        }
 
     private fun configuration(sourceTable: String, targetBucket: String): Configuration =
-            HBaseConfiguration.create().apply {
-                this["source.table"] = sourceTable
-                this["s3.bucket"] = targetBucket
-            }
+        HBaseConfiguration.create().apply {
+            this["source.table"] = sourceTable
+            this["s3.bucket"] = targetBucket
+        }
 
     private fun jobName(sourceTable: String) = "Snapshot exporter '$sourceTable': " +
             "'${SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())}'"
